6157cdd567904c1b03ed7aa2e3d7c34c2d4e3d26
Jian Tong <tj19832@gmail.com>
2019-05-14 13:58:05 +0800

    use binary instead of switch

--- src/main/java/org/katas/refactoring/fizzbuzz/switchstatement/FizzBuzz.java
+++ src/main/java/org/katas/refactoring/fizzbuzz/switchstatement/FizzBuzz.java
@@ -11,28 +11,31 @@ public class FizzBuzz {
         if (inputString.contains("3")) {
             return "Fizz";
         }
-        byte flag = 0;
-        flag += input % 3 == 0 ? 0b1 : 0;
-        flag += input % 5 == 0 ? 0b10 : 0;
-        flag += input % 7 == 0 ? 0b100 : 0;
+        int flag = 0;
+        flag += input % 3 == 0 ? 0b100 : 0;
+        flag += input % 5 == 0 ? 0b010 : 0;
+        flag += input % 7 == 0 ? 0b001 : 0;
 
-        switch (flag) {
-            case 1:
-                return "Fizz";
-            case 2:
-                return "Buzz";
-            case 3:
-                return "FizzBuzz";
-            case 4:
-                return "Whizz";
-            case 5:
-                return "FizzWhizz";
-            case 6:
-                return "BuzzWhizz";
-            case 7:
-                return "FizzBuzzWhizz";
+        String[] flags = getBinaryString(flag).split("");
+        String[] items = {"Fizz", "Buzz", "Whizz"};
+
+        for (int i = 0; i < flags.length; i++) {
+            if(flags[i].equals("1")){
+                result += items[i];
+            }
+        }
+        if (!"".equals(result)) {
+            return result;
         }
         return String.valueOf(input);
+    }
+
+    private String getBinaryString(int flag) {
+        String flagString = Integer.toBinaryString(flag);
+        while (flagString.length() < 3) {
+            flagString = "0"+flagString;
+        }
+        return flagString;
     }
 
 
@@ -41,5 +44,6 @@ public class FizzBuzz {
         for (int i = 0; i < 200; i++) {
             System.out.println(fizzBuzz.sayIt(i+1));
         }
+
     }
 }
